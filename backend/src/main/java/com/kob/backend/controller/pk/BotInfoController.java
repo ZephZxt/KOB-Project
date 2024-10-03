98b3fd81aa5771a919fff66f14be2c181eb40b46
Zeph <2356838436@qq.com>
2024-10-03 18:02:44 +0800

    环境搭建完成

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,5 +1,7 @@
 package com.kob.backend.controller.pk;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pk/")
 public class BotInfoController {
   @RequestMapping("getbotinfo/")
-  public String getBotInfo() {
-    return "hhhh";
+  public Map<String, String> getBotInfo() {
+    Map<String, String> bot1 = new HashMap<String, String>();
+    bot1.put("name", "tiger");
+    bot1.put("rating", "1500");
+    return bot1;
   }
 }
