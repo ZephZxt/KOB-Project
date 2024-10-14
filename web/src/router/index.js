341c1ad5a023d9aa9e776492ea4f7755c124b844
Zeph <2356838436@qq.com>
2024-10-14 10:43:40 +0800

    实现了导航栏和对战地图界面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,8 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import PkIndexView from '@/views/pk/PkIndexView.vue'
+import RankListIndexView from '@/views/ranklist/RankListIndexView.vue'
+import NotFound from '@/views/error/NotFound.vue'
+import RecordIndexView from '@/views/record/RecordIndexView.vue'
+import UserBotIndexView from '@/views/user/bot/UserBotIndexView.vue'
 
 const routes = [
-  
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/"
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RankListIndexView
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/404/"
+  }
   
 ]
 
