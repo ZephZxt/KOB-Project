b05177be0d814d258794f3236f1cf1adddf19020
Zeph <2356838436@qq.com>
2024-10-21 12:38:46 +0800

    配置Mysql与注册登录模块(中)

--- web/src/App.vue
+++ web/src/App.vue
@@ -6,10 +6,43 @@
 import NavBar from '@/components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
-
 export default {
   components :{
     NavBar
+  },
+  setup() {
+    
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/info/",
+    //   type:"GET",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlMWUwNWUzYzlhZDc0MmNjYmMxZmM3OGRmYjdhY2VhZiIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTcyOTM5MDUzNywiZXhwIjoxNzMwNjAwMTM3fQ.GP-w8_muncp1jIJrQgK475FM9l8bEKXGkdOolyc3eas"
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type:"POST",
+    //   data: {
+    //     username: "i",
+    //     password: "123",
+    //     confirmedPassword: "123"
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
+
   }
 }
 </script>
